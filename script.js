f3b05e8cf5d00133644a523780e3338a55d7aaf6
AlterX404 <109210388+AlterX404@users.noreply.github.com>
2026-07-24 19:00:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ const heroSection = document.querySelector(".hero");
 const heroTilt = document.querySelector(".hero-tilt");
 
 if (heroSection && heroTilt) {
-  const maximumRotation = 6;
+  const maximumRotation = 10;
 
   heroSection.addEventListener("pointermove", (event) => {
     if (event.pointerType === "touch") return;
